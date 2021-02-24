35da65408bbf8791fcfe8e1cd915a90e9d8ff5a8
Ville-Matias Heikkila <viznut@low.fi>
2021-02-24 12:05:57 +0200

    move the .txt sources into a separate directory

--- fixhex.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-
-$prevcp='';
-while(<>) {
-  ($cp,$bm)=split(':',$_);
-  if(length($cp)!=5) { $cp=substr('00000'.$cp,-5); }
-  print $cp.':'.$bm if($cp ne $prevcp);
-  $prevcp=$cp;
-}
